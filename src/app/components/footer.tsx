fb98720e3587184e8d3efab0000482ca05ff8a7c
Kristi Zentner <kristi.zentner17@gmail.com>
2023-10-27 14:08:06 -0700

    make footer layout adjustable to different resolutions

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
   };
 
   return (
-    <footer className='flex flex-col items-center gap-6 py-10'>
+    <footer className='flex flex-col items-center gap-4 py-10 md:flex-row'>
       {/* TODO: Add this about about me or in footer somewhere */}
       {/* Photo by{' '}
       <a
@@ -29,7 +29,7 @@ export default function Footer() {
       >
         Unsplash
       </a> */}
-      <ul className='text-zinc-600 text-sm text-center flex flex-col gap-2'>
+      <ul className='text-zinc-600 text-sm text-center flex flex-col gap-2 md:order-2'>
         <li>
           <a href='#about-me'>About me</a>
         </li>
@@ -43,29 +43,31 @@ export default function Footer() {
           <button onClick={toggleContactModal}>Contact me</button>
         </li>
       </ul>
-      <a
-        className='text-violet-500 border-b border-violet-500 mb-4'
-        href='mailto:me@kristizentner.com'
-        target='_blank'
-      >
-        me@kristizentner.com
-      </a>
-      <ul className='text-zinc-600 flex gap-2'>
-        <li>
-          <a
-            href='https://www.linkedin.com/in/kristin-zentner-651bb1112/'
-            target='_blank'
-            className='cursor-pointer'
-          >
-            <LinkedInIcon size={24} />
-          </a>
-        </li>
-        <li>
-          <a href='https://github.com/kfoxzz' target='_blank' className='cursor-pointer'>
-            <GithubIcon size={24} />
-          </a>
-        </li>
-      </ul>
+      <div className='flex flex-col gap-6 items-center md:order-1'>
+        <a
+          className='text-violet-500 border-b border-violet-500'
+          href='mailto:me@kristizentner.com'
+          target='_blank'
+        >
+          me@kristizentner.com
+        </a>
+        <ul className='text-zinc-600 flex gap-2'>
+          <li>
+            <a
+              href='https://www.linkedin.com/in/kristin-zentner-651bb1112/'
+              target='_blank'
+              className='cursor-pointer'
+            >
+              <LinkedInIcon size={24} />
+            </a>
+          </li>
+          <li>
+            <a href='https://github.com/kfoxzz' target='_blank' className='cursor-pointer'>
+              <GithubIcon size={24} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
