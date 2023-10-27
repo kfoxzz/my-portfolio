f51b4926354e0c129b5a1009e4d65822586758d2
Kristi Zentner <kristi.zentner17@gmail.com>
2023-10-26 21:57:41 -0700

    add social icons to hero

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,10 +1,29 @@
+import { FaGithubSquare as GithubIcon, FaLinkedin as LinkedInIcon } from 'react-icons/fa';
+
 export default function Hero() {
   return (
     <section className='relative p-8 pt-44 pb-24'>
       <h1 className="text-7xl font-bold tracking-wide pb-6 after:content-['Kristi'] after:text-violet-500">
         Hi, I&rsquo;m{' '}
       </h1>
-      <h2 className='text-2xl font-thin pb-16'>Software Engineer</h2>
+      <h2 className='text-2xl font-thin pb-4'>Software Engineer</h2>
+
+      <div className='flex pb-16 gap-2'>
+        <a
+          href='https://www.linkedin.com/in/kristin-zentner-651bb1112/'
+          target='_blank'
+          className='cursor-pointer hover:scale-110 transition duration-300 ease-in-out'
+        >
+          <LinkedInIcon size={24} />
+        </a>
+        <a
+          href='https://github.com/kfoxzz'
+          target='_blank'
+          className='cursor-pointer hover:scale-110 transition duration-300 ease-in-out'
+        >
+          <GithubIcon size={24} />
+        </a>
+      </div>
 
       <a
         className='bg-violet-500 text-black font-medium py-2 px-6 rounded hover:bg-violet-400 transition duration-300 ease-in-out'
